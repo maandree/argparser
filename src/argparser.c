225c4edb8a20b56d404a14050a8770801dac2976
Mattias Andrée <maandree@operamail.com>
2013-06-26 01:11:54 +0200

    move includes into new .h file
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/argparser.c
+++ src/argparser.c
@@ -16,9 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
+#include "argparser.h"
 
 
 /* Code style constants */
