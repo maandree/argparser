014af474e4267a2ad99c33bf744a3a876da03fc2
Mattias Andrée <maandree@operamail.com>
2013-06-30 11:27:33 +0200

    add package for java version
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/argparser/ArgParser.java
+++ src/argparser/ArgParser.java
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+package argparser;
+
 import java.util.*;
 import java.io.*;
 
